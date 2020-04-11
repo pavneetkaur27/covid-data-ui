9f90a4139a92cb45f4014b6c549e17591de59d31
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-04-11 11:34:13 +0530

    chnges

---
+++ client/build/precache-manifest.8af8e53d2808f534f805fbd922866093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084e8fb1b838db4ce22fd104039bb4f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d777ec5e4998169e320",
+    "url": "/static/css/main.2bcd089d.chunk.css"
+  },
+  {
+    "revision": "f8312f7466be97f0540e",
+    "url": "/static/js/2.460c50e8.chunk.js"
+  },
+  {
+    "revision": "3d777ec5e4998169e320",
+    "url": "/static/js/main.cd60f3fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "707852f13145a7f5606b4a83505faf6f",
+    "url": "/static/media/loader.707852f1.svg"
+  }
+]);
