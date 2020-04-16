7c6265b2e4ff372e848c3f02608e7358fa559465
pavneetkaur27 <pavneetkaur.jag@gmail.com>
2020-04-16 21:36:41 +0530

    dummy_script

---
+++ api/scripts/testingcrontab.js
@@ -0,0 +1,17 @@
+
+
+testing();
+
+
+function testing(finalCb){
+  console.log("hlllllllllllllllllloooooooooo");
+}
+
+
+
+
+function endScript() {
+    console.log('Exiting...');
+    return process.exit();
+}
+ 
